3345878ea6a71ce3cff4a6825bd96edcf9568027
Mohamed Abdelfattah <mohamed.adelhussein@gmail.com>
2019-04-09 22:02:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function shout (string) {return string.toUpperCase()}
 function whisper (string) {return string.toLowerCase()}
-function logShout(string) {return console.log(string.toUpperCase())
+function logShout(string) {return console.log(string.toUpperCase())}
+
   
