d8e5e07bcc876ccd7d72fd40b548e19e926dc7dc
Mohamed Abdelfattah <mohamed.adelhussein@gmail.com>
2019-04-09 22:35:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function whisper (string) {return string.toLowerCase()}
 function logShout(string) {return console.log(string.toUpperCase());}
 function logWhisper(string) {return console.log(string.toLowerCase());}
 
-function sayHiToGrandma (string) {if (string = string.toLowerCase())return "I can't hear you!";}
+function sayHiToGrandma (string) {if (string =string.toLowerCase())return "I can't hear you!";}
 function sayHiToGrandma (string) {if (string =string.toUpperCase())return 'YES INDEED!';}
-
+function sayHiToGrandma (string) {if (string ="I love you, Grandma.")() return "I love you, too.";}
 sayHiToGrandma("hello");
 sayHiToGrandma("HELLO");
 sayHiToGrandma("I love you, Grandma.");
